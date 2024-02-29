ae1aa4efee8f1c6d63df7c870ec3b69b8ecdd245
Obmazur <iamdinarium@gmail.com>
2024-02-29 12:56:07 +0300

    Заканчивате стилизацию главной, добавляет скрипт для модалки и состояния

--- source/script.js
+++ source/script.js
@@ -1,5 +1,9 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let modal = document.querySelector('.modal');
+let modalToggle = document.querySelector('.featured__button');
+let modalOverlay = document.querySelector('.modal__overlay');
+let page = document.querySelector('.page__body');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +14,21 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('main-nav--is-closed');
     navMain.classList.remove('main-nav--is-open');
+  }
+});
+
+modalToggle.addEventListener('click', function () {
+  if (modal.classList.contains('modal--is-closed')) {
+    modal.classList.remove('modal--is-closed');
+    modal.classList.add('modal--is-open');
+    page.classList.add('page__body--no-overflow');
+  }
+});
+
+modalOverlay.addEventListener('click', function () {
+  if (modal.classList.contains('modal--is-open')) {
+    modal.classList.remove('modal--is-open');
+    modal.classList.add('modal--is-closed');
+    page.classList.remove('page__body--no-overflow');
   }
 });
