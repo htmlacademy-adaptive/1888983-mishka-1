928472b601bbf8d0ec25381e6a18b78407f5cbf8
Alexander Mazur <iamdinarium@gmail.com>
2024-05-16 11:22:11 +0300

    Исправляет ошибки к защите

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,26 @@
+let site = document.querySelector('.page');
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 let modal = document.querySelector('.modal');
-let modalToggle = document.querySelector('.button--modal');
+let modalToggle = document.querySelectorAll('.button--modal');
 let modalOverlay = document.querySelector('.modal__overlay');
 let page = document.querySelector('.page__body');
 
-navMain.classList.remove('main-nav--nojs');
+site.classList.remove('nojs');
 
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--is-closed');
   navMain.classList.toggle('main-nav--is-open')
 });
 
-modalToggle.addEventListener('click', function () {
+modalToggle.forEach((element) => element.addEventListener('click', function (e) {
+  e.preventDefault();
   if (modal.classList.contains('modal--is-closed')) {
     modal.classList.remove('modal--is-closed');
     modal.classList.add('modal--is-open');
     page.classList.add('page__body--no-overflow');
   }
-});
+}));
 
 modalOverlay.addEventListener('click', function () {
   if (modal.classList.contains('modal--is-open')) {
